e5abc9219682241c16ee9c602451c9fd5f6dc817
김진아 <wlsdk7245@gmail.com>
2022-03-24 14:01:12 +0900

    chore: 개발 환경 구축

---
+++ 김진아/web-programming/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title'>안녕하세용~~</div>
+      <div className='content'>반갑습니당~~</div>
+    </div>
+  );
+}
+
+export default App;
