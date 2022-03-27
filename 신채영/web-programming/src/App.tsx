6499c7a07880266f6826c7051ee36b36b74cef11
신채영 <scy0723123@gmail.com>
2022-03-27 21:06:44 +0900

    test

---
+++ 신채영/web-programming/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="신채영">
+      <div className = 'title'> 신채영입니당ㅎㅎ </div>
+    </div>
+  );
+}
+
+export default App;
