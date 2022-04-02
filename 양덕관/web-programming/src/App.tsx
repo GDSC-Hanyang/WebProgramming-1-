467e4b5eda58c66b375711a7f46667f4b5fac244
dingding109 <ydk9819@gmail.com>
2022-04-02 12:39:14 +0900

    feat: create a react app

---
+++ 양덕관/web-programming/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hi">안녕하세요!!</div>
+      <div className="hi2">반갑습니다 ㅎㅎㅎㅎ</div>
+    </div>
+  );
+}
+
+export default App;
